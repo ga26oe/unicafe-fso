9013ac6c44c950584d425d4014b4a38b504b93fe
avi <gajai.avinash@gmail.com>
2026-01-22 06:10:35 -0500

    1.2

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -6,6 +6,8 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const all = good + neutral + bad
+
   return (
     <div>
       <h1>give feedback</h1>
@@ -13,19 +15,22 @@ const App = () => {
       <Button onClick={()=>setNeutral(neutral + 1)} text='nuetral' />
       <Button onClick={()=>setBad(bad + 1)} text='bad' />
       
-      <Statistics good={good} neutral={neutral} bad={bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} all = {all}/>
     </div>
   )
 }
 
 const Statistics = (props) => {
-  // conditional rendering?
   return (
     <div>
       <h1>statistics</h1>
       <p>good {props.good}</p>
       <p>nuetral {props.neutral}</p>
       <p>bad {props.bad}</p>
+      <p>all {props.good + props.neutral + props.bad}</p>
+
+      <p>average {(props.good - props.bad)/props.all}</p>
+      <p>positive {(props.good)/(props.all) * 100}% </p>
     </div>
   )
 }
